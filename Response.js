778bb3f3ec97770f1c652ff62d0d7f546ba6587a
Ashwin Jacob <ashjacob@gmail.com>
2022-03-04 15:44:12 -0600

    Code Run for google clone

---
+++ Response.js
@@ -0,0 +1,276 @@
+export default {
+    "kind": "customsearch#search",
+    "url": {
+        "type": "application/json",
+        "template": "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json"
+    },
+    "queries": {
+        "request": [
+            {
+                "title": "Google Custom Search - test",
+                "totalResults": "8",
+                "searchTerms": "test",
+                "count": 8,
+                "startIndex": 1,
+                "inputEncoding": "utf8",
+                "outputEncoding": "utf8",
+                "safe": "off",
+                "cx": "7e0623d6aef0628c1"
+            }
+        ]
+    },
+    "context": {
+        "title": "Google"
+    },
+    "searchInformation": {
+        "searchTime": 0.460771,
+        "formattedSearchTime": "0.46",
+        "totalResults": "8",
+        "formattedTotalResults": "8"
+    },
+    "items": [
+        {
+            "kind": "customsearch#result",
+            "title": "How Google's Algorithm is Focused on Its Users - Google Search",
+            "htmlTitle": "How Google&#39;s Algorithm is Focused on Its Users - Google Search",
+            "link": "https://www.google.com/search/howsearchworks/mission/users/",
+            "displayLink": "www.google.com",
+            "snippet": "Rigorous testing · Our goal is always to provide you the most useful and relevant information. Any changes we make to Search are always to improve the usefulness ...",
+            "htmlSnippet": "Rigorous <b>testing</b> &middot; Our goal is always to provide you the most useful and relevant information. Any changes we make to Search are always to improve the usefulness&nbsp;...",
+            "cacheId": "ObxN5sVIizUJ",
+            "formattedUrl": "https://www.google.com/search/howsearchworks/mission/users/",
+            "htmlFormattedUrl": "https://www.google.com/search/howsearchworks/mission/users/",
+            "pagemap": {
+                "metatags": [
+                    {
+                        "referrer": "no-referrer",
+                        "twitter:title": "How Google's Algorithm is Focused on Its Users - Google Search",
+                        "viewport": "width=device-width, initial-scale=1.0",
+                        "twitter:description": "Learn how changes to Google's algorithm are tested with Google search experiments to ensure that we continue to provide the most useful results, and how ads help us keep Google Search free.",
+                        "og:title": "How Google's Algorithm is Focused on Its Users - Google Search",
+                        "og:description": "Learn how changes to Google's algorithm are tested with Google search experiments to ensure that we continue to provide the most useful results, and how ads help us keep Google Search free."
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Android Beta Program",
+            "htmlTitle": "Android Beta Program",
+            "link": "https://www.google.com/android/beta",
+            "displayLink": "www.google.com",
+            "snippet": "Android Beta for Pixel offers you a simple way to try pre-release versions of Android, and test drive our new features. The feedback you provide will help ...",
+            "htmlSnippet": "Android Beta for Pixel offers you a simple way to try pre-release versions of Android, and <b>test</b> drive our new features. The feedback you provide will help&nbsp;...",
+            "cacheId": "AUFDLyB7DUsJ",
+            "formattedUrl": "https://www.google.com/android/beta",
+            "htmlFormattedUrl": "https://www.google.com/android/beta",
+            "pagemap": {
+                "metatags": [
+                    {
+                        "application-name": "Android Beta Program",
+                        "referrer": "origin",
+                        "apple-mobile-web-app-status-bar-style": "black",
+                        "msapplication-tap-highlight": "no",
+                        "viewport": "width=device-width, initial-scale=1.0",
+                        "apple-mobile-web-app-capable": "yes",
+                        "apple-mobile-web-app-title": "Android Beta Program",
+                        "mobile-web-app-capable": "yes"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Google",
+            "htmlTitle": "Google",
+            "link": "http://www.google.com/",
+            "displayLink": "www.google.com",
+            "snippet": "Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking ...",
+            "htmlSnippet": "Search the world&#39;s information, including webpages, images, videos and more. Google has many special features to help you find exactly what you&#39;re looking&nbsp;...",
+            "cacheId": "zhool8dxBV4J",
+            "formattedUrl": "www.google.com/",
+            "htmlFormattedUrl": "www.google.com/",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT3X9ZpkcNrr2kAcGZuXPZTNiK1oeTXhRMCZ1UGxNLf-an4du0eriX01mc",
+                        "width": "320",
+                        "height": "112"
+                    }
+                ],
+                "metatags": [
+                    {
+                        "referrer": "origin",
+                        "viewport": "width=device-width,minimum-scale=1.0",
+                        "google": "notranslate",
+                        "format-detection": "telephone=no"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "testing tool in Google Search Console",
+            "htmlTitle": "<b>testing</b> tool in Google Search Console",
+            "link": "https://www.google.com/webmasters/tools/robots-testing-tool",
+            "displayLink": "www.google.com",
+            "snippet": "Use Search Console to monitor Google Search results data for your properties.",
+            "htmlSnippet": "Use Search Console to monitor Google Search results data for your properties.",
+            "formattedUrl": "https://www.google.com/webmasters/tools/robots-testing-tool",
+            "htmlFormattedUrl": "https://www.google.com/webmasters/tools/robots-<b>testing</b>-tool",
+            "pagemap": {
+                "metatags": [
+                    {
+                        "viewport": "width=device-width, initial-scale=1",
+                        "format-detection": "telephone=no"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Rigorous Testing – How Google Search Works",
+            "htmlTitle": "Rigorous <b>Testing</b> – How Google Search Works",
+            "link": "https://www.google.com/search/howsearchworks/how-search-works/rigorous-testing/",
+            "displayLink": "www.google.com",
+            "snippet": "Improving Search with rigorous testing. Our goal is always to provide you with the most useful and relevant information. Any changes we make to Search are ...",
+            "htmlSnippet": "Improving Search with rigorous <b>testing</b>. Our goal is always to provide you with the most useful and relevant information. Any changes we make to Search are&nbsp;...",
+            "cacheId": "4VYSKZ0ZDVQJ",
+            "formattedUrl": "https://www.google.com/search/howsearchworks/how.../rigorous-testing/",
+            "htmlFormattedUrl": "https://www.google.com/search/howsearchworks/how.../rigorous-<b>testing</b>/",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGS3cygt5kGAiZY-7hO8fSAHJHy5cC3mlOzc5Dbg3h6qghrry_RtnEAEo",
+                        "width": "310",
+                        "height": "163"
+                    }
+                ],
+                "metatags": [
+                    {
+                        "og:image": "https://lh3.googleusercontent.com/dnpoiu9aopdadx61b_R8SlKyCHQIBNSTlYKOqUdZ1J8RPl3dGu1i57tmb7KzVEgpjdD_wc-CFirNtxU3wDiS9b1ZNAYxqq4tyojt5X4Q",
+                        "og:type": "website",
+                        "twitter:card": "summary",
+                        "twitter:title": "Rigorous Testing – How Google Search Works",
+                        "og:site_name": "Google Search - Discover How Google Search Works",
+                        "og:title": "Rigorous Testing – How Google Search Works",
+                        "og:description": "Our goal is always to provide you with the most useful and relevant information. Any changes we make to Search are always to improve the usefulness of results you see. That's why we never accept payment from anyone to be included in Search results.",
+                        "og:image:secure_url": "https://lh3.googleusercontent.com/dnpoiu9aopdadx61b_R8SlKyCHQIBNSTlYKOqUdZ1J8RPl3dGu1i57tmb7KzVEgpjdD_wc-CFirNtxU3wDiS9b1ZNAYxqq4tyojt5X4Q",
+                        "twitter:image": "https://lh3.googleusercontent.com/dnpoiu9aopdadx61b_R8SlKyCHQIBNSTlYKOqUdZ1J8RPl3dGu1i57tmb7KzVEgpjdD_wc-CFirNtxU3wDiS9b1ZNAYxqq4tyojt5X4Q",
+                        "referrer": "no-referrer",
+                        "viewport": "width=device-width, initial-scale=1.0",
+                        "twitter:description": "Our goal is always to provide you with the most useful and relevant information. Any changes we make to Search are always to improve the usefulness of results you see. That's why we never accept payment from anyone to be included in Search results.",
+                        "og:locale": "en",
+                        "og:url": "https://www.google.com/search/howsearchworks/how-search-works/rigorous-testing/"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://lh3.googleusercontent.com/dnpoiu9aopdadx61b_R8SlKyCHQIBNSTlYKOqUdZ1J8RPl3dGu1i57tmb7KzVEgpjdD_wc-CFirNtxU3wDiS9b1ZNAYxqq4tyojt5X4Q"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "ReCAPTCHA demo",
+            "htmlTitle": "ReCAPTCHA demo",
+            "link": "https://www.google.com/recaptcha/api2/demo",
+            "displayLink": "www.google.com",
+            "snippet": "Sample Form with ReCAPTCHA. First Name; Last Name; Email. Pick your favorite color: Red Green.",
+            "htmlSnippet": "Sample Form with ReCAPTCHA. First Name; Last Name; Email. Pick your favorite color: Red Green.",
+            "cacheId": "P8RKCmSVBegJ",
+            "formattedUrl": "https://www.google.com/recaptcha/api2/demo",
+            "htmlFormattedUrl": "https://www.google.com/recaptcha/api2/demo",
+            "pagemap": {
+                "metatags": [
+                    {
+                        "viewport": "width=device-width, user-scalable=yes"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Think with Google - Discover Marketing Research & Digital Trends",
+            "htmlTitle": "Think with Google - Discover Marketing Research &amp; Digital Trends",
+            "link": "https://www.google.com/think/",
+            "displayLink": "www.google.com",
+            "snippet": "Tools · Test My Site. Test your mobile site speed and deliver a better experience. Open · Find My Audience. Go beyond demographics to find who matters most to ...",
+            "htmlSnippet": "Tools &middot; <b>Test</b> My Site. <b>Test</b> your mobile site speed and deliver a better experience. Open &middot; Find My Audience. Go beyond demographics to find who matters most to&nbsp;...",
+            "cacheId": "ZPw1L_Ri1DYJ",
+            "formattedUrl": "https://www.google.com/think/",
+            "htmlFormattedUrl": "https://www.google.com/think/",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTLlV_xk_vh2_Vq2XQe9ZLBx-GYFuCp9BKnY68pjPiFXdw34pwp9TARl10",
+                        "width": "300",
+                        "height": "168"
+                    }
+                ],
+                "metatags": [
+                    {
+                        "og:image": "/_qs/static/img/global/twg-logo-large.jpg",
+                        "og:type": "website",
+                        "twitter:card": "summary_large_image",
+                        "twitter:title": "Think with Google - Discover Marketing Research & Digital Trends",
+                        "og:site_name": "Think with Google",
+                        "twitter:site": "@ThinkwithGoogle",
+                        "viewport": "width=device-width, initial-scale=1",
+                        "twitter:description": "Uncover the latest marketing research and digital trends with data reports, guides, infographics, and articles from Think with Google.",
+                        "og:title": "Think with Google - Discover Marketing Research & Digital Trends",
+                        "og:url": "https://www.thinkwithgoogle.com/",
+                        "og:description": "Uncover the latest marketing research and digital trends with data reports, guides, infographics, and articles from Think with Google.",
+                        "twitter:image": "/_qs/static/img/global/twg-logo-large.jpg"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://storage.googleapis.com/twg-content/original_images/2151-Infographic-Finance_THUMB.gif"
+                    }
+                ]
+            }
+        },
+        {
+            "kind": "customsearch#result",
+            "title": "Google Maps",
+            "htmlTitle": "Google Maps",
+            "link": "https://www.google.com/maps/",
+            "displayLink": "www.google.com",
+            "snippet": "Your location. Trails. Dedicated lanes. Bicycle-friendly roads. Dirt/unpaved trails. Live traffic. Fast. Slow. Wildfires Info. 2000 km. Traffic Transit",
+            "htmlSnippet": "Your location. Trails. Dedicated lanes. Bicycle-friendly roads. Dirt/unpaved trails. Live traffic. Fast. Slow. Wildfires Info. 2000 km. Traffic Transit",
+            "cacheId": "LGv4n99UVyoJ",
+            "formattedUrl": "https://www.google.com/maps/",
+            "htmlFormattedUrl": "https://www.google.com/maps/",
+            "pagemap": {
+                "cse_thumbnail": [
+                    {
+                        "src": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQUDTMfgWSotdHvbOKW4AmY1steqsilnDJS4q33zGx-d0AXvvxPiFY9Mvs",
+                        "width": "204",
+                        "height": "247"
+                    }
+                ],
+                "metatags": [
+                    {
+                        "referrer": "origin",
+                        "twitter:card": "summary",
+                        "theme-color": "#3367d6",
+                        "viewport": "initial-scale=1.0, user-scalable=no",
+                        "google": "notranslate"
+                    }
+                ],
+                "cse_image": [
+                    {
+                        "src": "https://maps.gstatic.com/tactile/basepage/pegman_sherlock.png"
+                    }
+                ]
+            }
+        }
+    ]
+}
---
+++ pages/search.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Head from 'next/head';
+import Header from '../components/Header';
+import { API_KEY, CONTEXT_KEY } from '../keys';
+import Response from '../response'
+import { useRouter } from 'next/router';
+import SearchResults from '../components/SearchResults';
+
+const Search = ({results}) => {
+    const router = useRouter();
+    return (
+        <div>
+            <Head>
+                <title>{router.query.term}-Google Search</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            {/* Headers */}
+            <Header />
+            {/* Search Results */}
+            <SearchResults results={results} />
+        </div>
+    );
+};
+
+export default Search;
+
+export async function getServerSideProps(context) {
+    const useDummyData = false;
+    const startIndex = context.query.start || "0";
+
+
+const data = useDummyData ? Response  : await fetch(`https://customsearch.googleapis.com/customsearch/v1
+?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)
+    .then(response => response.json());
+
+    return {
+        props: {
+            results: data
+        }
+    }
+
+}
